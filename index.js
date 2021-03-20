dd0ad4d607c5392c474815a4cbe9710111b14907
foresightyj <imyuanjian@gmail.com>
2021-03-20 09:29:12 +0800

    fix array reverse mutation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 /** @type {import("assert")} */
 const assert = require("assert");
 const path = require("path");
+const _ = require("lodash");
 const shelljs = require("shelljs");
 const fs = require("fs-extra");
 const opn = require("opn");
@@ -24,10 +25,10 @@ program
   .option("-f --toFile", "是否将结果写入到文件中，默认是打印到stdout")
   .option("-o --openInBrowser", "是否在浏览器里面打开对应的airflow web页面")
   .option("-a --avajsStat", "是否统计avajs结果")
-  .option(
-    "-i --keepInfo",
-    "是否需要保留airflow日志里面的INFO行，默认是过滤掉INFO行"
-  )
+  //   .option(
+  //     "-i --keepInfo",
+  //     "是否需要保留airflow日志里面的INFO行，默认是过滤掉INFO行"
+  //   )
   .parse(process.argv);
 
 /** @type {string} */
@@ -44,7 +45,7 @@ assert(baseURL, "baseURL is falsy");
 
 const TO_FILE = Boolean(program.toFile);
 const AVAJS_STAT = Boolean(program.avajsStat);
-const KEEP_INFO = Boolean(program.keepInfo);
+// const KEEP_INFO = Boolean(program.keepInfo);
 const OPEN_IN_BROWSER = Boolean(program.openInBrowser);
 
 const http = axios.create({ baseURL });
@@ -60,8 +61,8 @@ const http = axios.create({ baseURL });
 async function getLatestRunOfDag(dagPrefix) {
   const res = await http.get("/api/experimental/latest_runs");
   const d = res.data;
+  assert.strictEqual(res.status, 200, `get latest_runs returned ${res.status}`);
   /** @type {Dag} */
-  console.log(d);
   const dag = d.items.find((d) => d.dag_id.startsWith(dagPrefix));
   assert(dag.dag_id, "dag_id is falsy");
   return dag;
@@ -83,19 +84,13 @@ async function getDagRunLog(dagId, taskId, execution_date) {
   assert(content, "content falsy");
   assert(typeof content === "string", "content not string");
   let lines = content.split("\n");
-  if (!KEEP_INFO) {
-    lines = lines.filter((l) => !l.includes("INFO"));
-  }
+  lines = lines.filter((l) => !l.includes(": Subtask "));
   const firstLine = lines.find((l) => l.startsWith("[202"));
-  const lastLine = lines.reverse().find((l) => l.startsWith("[202"));
-  const smartEnd = lines.findIndex((l) =>
-    l.includes("npm ERR! This is probably not a problem with npm")
-  );
-  if (smartEnd > -1) {
-    lines = lines.slice(0, smartEnd);
-  }
+  //_.reverse also mutates array
+  const lastLine = _.reverse(lines.slice()).find((l) => l.startsWith("[202"));
   content = lines.join("\n");
   if (TO_FILE) {
+    console.log(firstLine, lastLine, content);
     const reportDir = path.join(__dirname, "reports");
     await fs.ensureDir(reportDir);
     const filePath = path.join(reportDir, `${taskId}.log`);
@@ -104,51 +99,53 @@ async function getDagRunLog(dagId, taskId, execution_date) {
     });
     opn(filePath);
 
-    let deltaSeconds = 0;
-    /**
-     * @param {string} s
-     * @returns {Date}
-     */
-    function parseTime(s) {
-      const t = s.split("]")[0].substr(1);
-      return new Date(t.split(",")[0]);
-    }
-    if (firstLine && lastLine.includes("[202")) {
-      const startTime = parseTime(firstLine);
-      const endTime = parseTime(lastLine);
-      const deltaMs = endTime.getTime() - startTime.getTime();
-      deltaSeconds = Number((deltaMs / 1000).toFixed(0));
-    }
+    if (AVAJS_STAT) {
+      let deltaSeconds = 0;
+      /**
+       * @param {string} s
+       * @returns {Date}
+       */
+      function parseTime(s) {
+        const t = s.split("]")[0].substr(1);
+        return new Date(t.split(",")[0]);
+      }
+      if (firstLine && lastLine.includes("[202")) {
+        const startTime = parseTime(firstLine);
+        const endTime = parseTime(lastLine);
+        const deltaMs = endTime.getTime() - startTime.getTime();
+        deltaSeconds = Number((deltaMs / 1000).toFixed(0));
+      }
 
-    const oks = shelljs
-      .grep("INFO -   ✔", filePath)
-      .split("\n")
-      .filter((s) => s.trim());
-    const attentions = shelljs
-      .grep("INFO -   ✖", filePath)
-      .split("\n")
-      .filter((s) => s.trim());
-    const okCount = oks.length;
-    const failCount = attentions.length;
+      const oks = shelljs
+        .grep("INFO -   ✔", filePath)
+        .split("\n")
+        .filter((s) => s.trim());
+      const attentions = shelljs
+        .grep("INFO -   ✖", filePath)
+        .split("\n")
+        .filter((s) => s.trim());
+      const okCount = oks.length;
+      const failCount = attentions.length;
 
-    const attentionFilePath = `./reports/${taskId}.attention-${failCount}_${
-      failCount + okCount
-    }.log`;
+      const attentionFilePath = `./reports/${taskId}.attention-${failCount}_${
+        failCount + okCount
+      }.log`;
 
-    const completed = content.includes("Task exited with return code");
-    await fs.promises.writeFile(
-      attentionFilePath,
-      `======================================================\r\nTOTAL: ${
-        okCount + failCount
-      } PASS: ${okCount}, FAIL: ${failCount}\r\n
+      const completed = content.includes("Task exited with return code");
+      await fs.promises.writeFile(
+        attentionFilePath,
+        `======================================================\r\nTOTAL: ${
+          okCount + failCount
+        } PASS: ${okCount}, FAIL: ${failCount}\r\n
 Finished: ${completed}, Time Taken ${deltaSeconds} seconds
 ======================================================\r\n` +
-        attentions.join("\r\n"),
-      {
-        encoding: "utf-8",
-      }
-    );
-    opn(attentionFilePath);
+          attentions.join("\r\n"),
+        {
+          encoding: "utf-8",
+        }
+      );
+      opn(attentionFilePath);
+    }
   } else {
     console.log(content);
   }
