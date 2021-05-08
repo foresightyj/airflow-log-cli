1aa279157733291b4177d4f2bf421deb3c59160a
foresightyj <imyuanjian@gmail.com>
2021-05-08 16:50:15 +0800

    updated for airflow 2

--- ReadMe.md
+++ ReadMe.md
@@ -10,7 +10,7 @@ Options:
   -V, --version            output the version number
   -d, --dagId <dagId>
   -r, --runId <runId>
-  -b, --baseUrl <baseUrl>  baseUrl of airflow (default: "http://192.168.0.229:8081")
+  -b, --baseUrl <baseUrl>  baseUrl of airflow (default: "http://192.168.0.229:8080")
   -f --toFile              是否将结果写入到文件中，默认是打印到stdout
   -i --keepInfo            是否需要保留airflow日志里面的INFO行，默认是过滤掉INFO行
   -h, --help               display help for command
--- index.js
+++ index.js
@@ -11,33 +11,34 @@ const opn = require("opn");
 const program = require("commander");
 const axios = require("axios").default;
 const package = require("./package.json");
+const { runInContext } = require("lodash");
 
 program
-  .name(package.name)
-  .version(package.version)
-  .requiredOption("-d, --dagId <dagId>")
-  .requiredOption("-r, --runId <runId>")
-  .requiredOption(
-    "-b, --baseUrl <baseUrl>",
-    "baseUrl of airflow",
-    "http://192.168.0.229:8081"
-  )
-  .option("-f --toFile", "是否将结果写入到文件中，默认是打印到stdout")
-  .option("-o --openInBrowser", "是否在浏览器里面打开对应的airflow web页面")
-  .option("-a --avajsStat", "是否统计avajs结果")
-  //   .option(
-  //     "-i --keepInfo",
-  //     "是否需要保留airflow日志里面的INFO行，默认是过滤掉INFO行"
-  //   )
-  .parse(process.argv);
+    .name(package.name)
+    .version(package.version)
+    .requiredOption("-d, --dagId <dagId>")
+    .requiredOption("-t, --taskId <taskId>")
+    .requiredOption(
+        "-b, --baseUrl <baseUrl>",
+        "baseUrl of airflow",
+        "http://192.168.0.229:8080"
+    )
+    .option("-f --toFile", "是否将结果写入到文件中，默认是打印到stdout")
+    .option("-o --openInBrowser", "是否在浏览器里面打开对应的airflow web页面")
+    .option("-a --avajsStat", "是否统计avajs结果")
+    //   .option(
+    //     "-i --keepInfo",
+    //     "是否需要保留airflow日志里面的INFO行，默认是过滤掉INFO行"
+    //   )
+    .parse(process.argv);
 
 /** @type {string} */
 const dagId = program.dagId;
 assert(dagId, "dagId is falsy");
 
 /** @type {string} */
-const runId = program.runId;
-assert(runId, "runId is falsy");
+const taskId = program.taskId;
+assert(taskId, "taskId is falsy");
 
 /** @type {string} */
 const baseURL = program.baseUrl;
@@ -48,124 +49,135 @@ const AVAJS_STAT = Boolean(program.avajsStat);
 // const KEEP_INFO = Boolean(program.keepInfo);
 const OPEN_IN_BROWSER = Boolean(program.openInBrowser);
 
-const http = axios.create({ baseURL });
+const http = axios.create({
+    baseURL,
+    auth: {
+        username: "airflow",
+        password: "airflow"
+    }
+});
 
 /**
- * @typedef {{dag_id:string, dag_run_url:string, execution_date:string, start_date:string}} Dag
- */
-
-/**
- * @param {string} dagPrefix
- * @returns {Promise<Dag>}
- */
-async function getLatestRunOfDag(dagPrefix) {
-  const res = await http.get("/api/experimental/latest_runs");
-  const d = res.data;
-  assert.strictEqual(res.status, 200, `get latest_runs returned ${res.status}`);
-  /** @type {Dag} */
-  const dag = d.items.find((d) => d.dag_id.startsWith(dagPrefix));
-  assert(dag.dag_id, "dag_id is falsy");
-  return dag;
-}
+ * @typedef {{dag_id:string, dag_run_id: string, dag_run_url:string, execution_date:string, start_date:string, state: "failed"}} DagRun
+ * @typedef {{ dag_id: string, duration: number, end_date: string, execution_date: string, executor_config: string, hostname: string, max_tries: number, operator: string, pid: number, pool: string, pool_slots: number, priority_weight: number, queue: string, queued_when: string, start_date: string, state: string, task_id: string, try_number: number, unixname: string,}} TaskInstance */
 
 /**
  * @param {string} dagId
- * @param {string} taskId
- * @param {string} execution_date
+ * @returns {Promise<DagRun>}
  */
-async function getDagRunLog(dagId, taskId, execution_date) {
-  const url = `/admin/airflow/get_logs_with_metadata?task_id=${taskId}&dag_id=${dagId}&execution_date=${encodeURIComponent(
-    execution_date
-  )}&try_number=1&metadata=null`;
-  const res = await http.get(url);
-  const data = res.data;
-  /** @type {string} */
-  let content = data.message;
-  assert(content, "content falsy");
-  assert(typeof content === "string", "content not string");
-  let lines = content.split("\n");
-  lines = lines.filter((l) => !l.includes(": Subtask "));
-  const firstLine = lines.find((l) => l.startsWith("[202"));
-  //_.reverse also mutates array
-  const lastLine = _.reverse(lines.slice()).find((l) => l.startsWith("[202"));
-  content = lines.join("\n");
-  if (TO_FILE) {
-    console.log(firstLine, lastLine, content);
-    const reportDir = path.join(__dirname, "reports");
-    await fs.ensureDir(reportDir);
-    const filePath = path.join(reportDir, `${taskId}.log`);
-    await fs.promises.writeFile(filePath, content, {
-      encoding: "utf-8",
+async function getLatestRunOfDag(dagId) {
+    const res = await http.get(`/api/v1/dags/${dagId}/dagRuns`, {
+        params: {
+            limit: 1,
+            order_by: "-start_date",
+        }
     });
-    opn(filePath);
+    const d = res.data;
+    assert.strictEqual(res.status, 200, `get latest_runs returned ${res.status}`);
+    /** @type {DagRun} */
+    const dag = d.dag_runs[0]
+    assert(dag.dag_id, "dag_id is falsy");
+    return dag;
+}
 
-    if (AVAJS_STAT) {
-      let deltaSeconds = 0;
-      /**
-       * @param {string} s
-       * @returns {Date}
-       */
-      function parseTime(s) {
-        const t = s.split("]")[0].substr(1);
-        return new Date(t.split(",")[0]);
-      }
-      if (firstLine && lastLine.includes("[202")) {
-        const startTime = parseTime(firstLine);
-        const endTime = parseTime(lastLine);
-        const deltaMs = endTime.getTime() - startTime.getTime();
-        deltaSeconds = Number((deltaMs / 1000).toFixed(0));
-      }
+/**
+ * @param {DagRun} dagRun
+ * @param {string} taskId
+ * @returns {Promise<TaskInstance>}
+ */
+async function getTaskInstance(dagRun, taskId) {
+    const url = `/api/v1/dags/${dagRun.dag_id}/dagRuns/${dagRun.dag_run_id}/taskInstances`
+    const res = await http.get(url);
+    const data = res.data;
+    const task = data.task_instances.find(inst => inst.task_id === taskId);
+    return task;
+}
 
-      const oks = shelljs
-        .grep("INFO -   ✔", filePath)
-        .split("\n")
-        .filter((s) => s.trim());
-      const attentions = shelljs
-        .grep("INFO -   ✖", filePath)
-        .split("\n")
-        .filter((s) => s.trim());
-      const okCount = oks.length;
-      const failCount = attentions.length;
+/**
+ * @param {TaskInstance} task
+ * @param {string} runId
+ */
+async function getTaskLog(task, runId) {
+    const url = `/api/v1/dags/${task.dag_id}/dagRuns/${runId}/taskInstances/${task.task_id}/logs/${task.try_number}`
+    const res = await http.get(url);
+    const data = res.data;
+    /** @type {string} */
+    assert(typeof data === "string", "data not string");
+    let lines = data.split("\n");
+    lines = lines.filter((l) => !l.includes(": Subtask "));
+    const firstLine = lines.find((l) => l.startsWith("[202"));
+    //_.reverse also mutates array
+    const lastLine = _.reverse(lines.slice()).find((l) => l.startsWith("[202"));
+    const content = lines.join("\n");
+    if (TO_FILE) {
+        console.log(firstLine, lastLine, content);
+        const reportDir = path.join(__dirname, "reports");
+        const filePath = path.join(reportDir, `${task.task_id}.log`);
+        await fs.outputFile(filePath, content, {
+            encoding: "utf-8",
+        });
+        opn(filePath);
 
-      const attentionFilePath = `./reports/${taskId}.attention-${failCount}_${
-        failCount + okCount
-      }.log`;
+        if (AVAJS_STAT) {
+            let deltaSeconds = 0;
+            /**
+             * @param {string} s
+             * @returns {Date}
+             */
+            function parseTime(s) {
+                const t = s.split("]")[0].substr(1);
+                return new Date(t.split(",")[0]);
+            }
+            if (firstLine && lastLine.includes("[202")) {
+                const startTime = parseTime(firstLine);
+                const endTime = parseTime(lastLine);
+                const deltaMs = endTime.getTime() - startTime.getTime();
+                deltaSeconds = Number((deltaMs / 1000).toFixed(0));
+            }
 
-      const completed = content.includes("Task exited with return code");
-      await fs.promises.writeFile(
-        attentionFilePath,
-        `======================================================\r\nTOTAL: ${
-          okCount + failCount
-        } PASS: ${okCount}, FAIL: ${failCount}\r\n
+            const oks = shelljs
+                .grep("INFO -   ✔", filePath)
+                .split("\n")
+                .filter((s) => s.trim());
+            const attentions = shelljs
+                .grep("INFO -   ✖", filePath)
+                .split("\n")
+                .filter((s) => s.trim());
+            const okCount = oks.length;
+            const failCount = attentions.length;
+
+            const attentionFilePath = `./reports/${runId}.attention-${failCount}_${failCount + okCount
+                }.log`;
+
+            const completed = content.includes("Task exited with return code");
+            await fs.promises.writeFile(
+                attentionFilePath,
+                `======================================================\r\nTOTAL: ${okCount + failCount
+                } PASS: ${okCount}, FAIL: ${failCount}\r\n
 Finished: ${completed}, Time Taken ${deltaSeconds} seconds
 ======================================================\r\n` +
-          attentions.join("\r\n"),
-        {
-          encoding: "utf-8",
+                attentions.join("\r\n"),
+                {
+                    encoding: "utf-8",
+                }
+            );
+            opn(attentionFilePath);
         }
-      );
-      opn(attentionFilePath);
+    } else {
+        console.log(content);
     }
-  } else {
-    console.log(content);
-  }
 
-  const webUrl =
-    baseURL +
-    `/admin/airflow/graph?dag_id=${dagId}&execution_date=${encodeURIComponent(
-      execution_date
-    )}`;
+    const webUrl = baseURL + `/log?dag_id=${dagId}&task_id=${task.task_id}&execution_date=${encodeURIComponent(task.execution_date)}`;
 
-  if (OPEN_IN_BROWSER) {
-    opn(webUrl);
-  }
+    if (OPEN_IN_BROWSER) {
+        opn(webUrl);
+    }
 }
 
 (async () => {
-  const dag = await getLatestRunOfDag(dagId);
-  const runUrl = new URL(baseURL + dag.dag_run_url);
-  const params = runUrl.searchParams;
-  const execution_date = params.get("execution_date");
-  assert(execution_date, "execution_date is falsy");
-  await getDagRunLog(dag.dag_id, runId, execution_date);
+    const dag = await getLatestRunOfDag(dagId);
+    assert(dag.execution_date, "execution_date is falsy");
+    const task = await getTaskInstance(dag, taskId);
+    const log = await getTaskLog(task, dag.dag_run_id);
+    console.log("LOG", log);
 })();
